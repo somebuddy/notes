682567e6d0dbef0f435318f476f6e9d84f4aee51
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-04 13:36:33 +0000

    Add box base model

--- client/box.ts
+++ client/box.ts
@@ -1,4 +1,13 @@
 export class Box {
   id: number;
+  boxType: string;
   title: string;
+  content: string;
+  states: Object;
+
+  parent: string;
+  children: string[];
+
+  createdAt: Date;
+  updatedAt: Date;
 }
--- client/mock-boxes.ts
+++ client/mock-boxes.ts
@@ -1,9 +1,11 @@
 import {Box} from './box';
 
 export var BOXES: Box[] = [
-  { "id": 1, "title": "Sample"},
-  { "id": 2, "title": "Sample 2"},
-  { "id": 3, "title": "Sample 3"},
-  { "id": 4, "title": "Sample 4"},
-  { "id": 5, "title": "Sample 5"},
+  { "id": 1, "title": "Sample", "states": {}, parent: undefined, content: undefined, children: [], boxType: 'note', createdAt: new Date(), updatedAt: new Date()},
+  { "id": 2, "title": "Sample 2", "states": {}, parent: undefined, content: undefined, children: [], boxType: 'note', createdAt: new Date(), updatedAt: new Date()},
+  { "id": 3, "title": "Sample 3", "states": {}, parent: undefined, content: undefined, children: [], boxType: 'note', createdAt: new Date(), updatedAt: new Date()},
+  { "id": 4, "title": "Sample 4", "states": {}, parent: undefined, content: undefined, children: [], boxType: 'note', createdAt: new Date(), updatedAt: new Date()},
+  { "id": 5, "title": "Sample 5", "states": {}, parent: undefined, content: undefined, children: [], boxType: 'note', createdAt: new Date(), updatedAt: new Date()},
 ]
+
+BOXES[0].states["deleted"] = true;
