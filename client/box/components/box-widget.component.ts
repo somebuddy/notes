0796133f6ec111a27d72d702b67cb706f35a39e3
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-05 08:33:46 +0000

    Add box widget template

--- client/box/components/box-widget.component.ts
+++ client/box/components/box-widget.component.ts
@@ -6,13 +6,7 @@ import { BoxService } from '../services/box.service';
 
 @Component({
   selector: 'box-widget',
-  template: `
-    <li class="box-widget" [class.deleted]="box.states.deleted">
-      <span class="title">{{ box.title }}</span>
-      <button class="delete-button" (click)="deleteBox()">Delete</button>
-      <button class="details-button" (click)="gotoDetail()">View details</button>
-    </li>
-  `
+  templateUrl: './box/components/box-widget.component.html'
 })
 export class BoxWidgetComponent {
   @Input() box: Box;
