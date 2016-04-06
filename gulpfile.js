c758f066bdf42aaf06127219abc43ea84cbea558
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-06 12:50:56 +0000

    Add copy html to build gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,29 @@ var browserSync = require('browser-sync').create();
 var serveConfig = require('./configs/bs-config.js');
 
 
-gulp.task('serve', function() {
+gulp.task('serve', ['build'], function() {
   console.log('Gulp serving...');
   browserSync.init(serveConfig);
 });
 
-gulp.task('build:client', function() {
-    var tsProject = ts.createProject('tsconfig.json');
-    tsProject.src().pipe(ts(tsProject)).js.pipe(gulp.dest('build'));
-})
+gulp.task('build:client:typescript', function() {
+  var tsProject = ts.createProject('tsconfig.json');
+  tsProject.src().pipe(ts(tsProject)).js.pipe(gulp.dest('build'));
+});
+
+gulp.task('build:client:html', function() {
+  console.log('Preparing html');
+  gulp.src('client/**/*.html')
+    .pipe(gulp.dest('build/client'));
+});
+
+gulp.task('build:client', [
+  'build:client:typescript',
+  'build:client:html'
+]);
+
+gulp.task('build:server', []);
+
+gulp.task('build', ['build:client', 'build:server']);
 
 gulp.task('default', ['serve']);
