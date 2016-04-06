0c86b77b83d3b3bb62d0e5e68db19bc476028b9b
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-06 13:29:50 +0000

    Add cleaning build folder task before compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 var gulp = require('gulp');
+var del = require('del');
 var ts = require('gulp-typescript');
 var sass = require('gulp-sass');
 
 var browserSync = require('browser-sync').create();
 var serveConfig = require('./configs/bs-config.js');
 
-gulp.task('build:client:typescript', function() {
+gulp.task('clean:build', function() {
+  console.log('Cleaning previous builds...');
+  return del(['./build/**/*']);
+});
+
+gulp.task('build:client:typescript', ['clean:build'], function() {
+  console.log('Compile typescript...');
   var tsProject = ts.createProject('tsconfig.json');
   tsProject.src().pipe(ts(tsProject)).js.pipe(gulp.dest('build'));
 });
 
-gulp.task('build:client:html', function() {
-  console.log('Preparing html');
+gulp.task('build:client:html', ['clean:build'], function() {
+  console.log('Preparing html...');
   gulp.src('client/**/*.html')
     .pipe(gulp.dest('./build/client'));
 });
 
-gulp.task('build:client:sass', function() {
+gulp.task('build:client:sass', ['clean:build'], function() {
+  console.log('Compile sass files...');
   gulp.src('styles/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/styles'));
@@ -24,13 +32,16 @@ gulp.task('build:client:sass', function() {
 
 gulp.task('build:client', [
   'build:client:typescript',
-  'build:client:html',
-  'build:client:sass'
+  'build:client:sass',
+  'build:client:html'
 ]);
 
 gulp.task('build:server', []);
 
-gulp.task('build', ['build:client', 'build:server']);
+gulp.task('build', [
+  'build:client',
+  'build:server'
+]);
 
 gulp.task('serve', ['build'], function() {
   console.log('Gulp serving...');
