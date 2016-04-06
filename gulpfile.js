4220a4f4705c7ec6c057d8b726b8a66530cdbd7f
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-06 13:11:26 +0000

    Add gulp build sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,9 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var sass = require('gulp-sass');
 
 var browserSync = require('browser-sync').create();
 var serveConfig = require('./configs/bs-config.js');
-
-
-gulp.task('serve', ['build'], function() {
-  console.log('Gulp serving...');
-  browserSync.init(serveConfig);
-});
 
 gulp.task('build:client:typescript', function() {
   var tsProject = ts.createProject('tsconfig.json');
@@ -18,16 +13,28 @@ gulp.task('build:client:typescript', function() {
 gulp.task('build:client:html', function() {
   console.log('Preparing html');
   gulp.src('client/**/*.html')
-    .pipe(gulp.dest('build/client'));
+    .pipe(gulp.dest('./build/client'));
+});
+
+gulp.task('build:client:sass', function() {
+  gulp.src('styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/styles'));
 });
 
 gulp.task('build:client', [
   'build:client:typescript',
-  'build:client:html'
+  'build:client:html',
+  'build:client:sass'
 ]);
 
 gulp.task('build:server', []);
 
 gulp.task('build', ['build:client', 'build:server']);
+
+gulp.task('serve', ['build'], function() {
+  console.log('Gulp serving...');
+  browserSync.init(serveConfig);
+});
 
 gulp.task('default', ['serve']);
