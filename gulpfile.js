a62990aad55cf6f46cab79872b4f7459f3d0fab2
Somebuddy <oleksiy.drogovozov@gmail.com>
2016-04-06 03:09:52 +0000

    Add gulp typescript compile task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
 
 var browserSync = require('browser-sync').create();
 var serveConfig = require('./configs/bs-config.js');
+
 
 gulp.task('serve', function() {
   console.log('Gulp serving...');
   browserSync.init(serveConfig);
 });
+
+gulp.task('build:client', function() {
+    var tsProject = ts.createProject('tsconfig.json');
+    tsProject.src().pipe(ts(tsProject)).js.pipe(gulp.dest('build'));
+})
 
 gulp.task('default', ['serve']);
